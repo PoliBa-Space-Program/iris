2c4583794e841de0a0fecf8ddad696ca01e837ff
oscar0urselli <urselli.oscar@gmail.com>
2024-07-24 19:51:47 +0200

    docs: [refactoring] changes to README.md and version to 3.0.0

--- examples/rust/iris.rs
+++ examples/rust/iris.rs
@@ -28,56 +28,6 @@ _ => panic!("No variant found.")}
 }
 }
 #[derive(Copy, Clone)]
-pub struct Data {
-pub computers: [Computer; 2],
-}
-impl Data {
-pub const NAME_HASH: u32 = 1062369733;
-pub const BYTES_LENGTH: usize = 32 + 4;
-pub fn encode(&self) -> [u8; Data::BYTES_LENGTH] {
-let mut data: [u8; Data::BYTES_LENGTH] = [0; Data::BYTES_LENGTH];let mut index = 0;for x in u32::to_be_bytes(Data::NAME_HASH) {
-data[index] = x;
-index += 1;
-}
-for x in self.to_be_bytes() {
-data[index] = x;
-index += 1;
-}
-data
-}
-pub fn to_be_bytes(&self) -> [u8; Data::BYTES_LENGTH - 4] {
-let mut data: [u8; Data::BYTES_LENGTH - 4] = [0; Data::BYTES_LENGTH - 4];let mut index = 0;for i in self.computers {
-for x in i.to_be_bytes() {
-data[index] = x;
-index += 1;
-}
-}
-data
-}
-pub fn decode(data: &[u8]) -> Data {
-Data::from_be_bytes(data[4..data.len()].try_into().unwrap())
-}
-pub fn from_be_bytes(data: [u8; Data::BYTES_LENGTH - 4]) -> Data {
-let mut out = Data {
-computers: [Computer {
-batteries: [Battery {
-charge: 0.0,
-}
-; 2],
-status: Status::SLEEP,
-id: 0,
-}
-; 2],
-};
-let mut index = 0;
-for i in 0..2 {
-out.computers[i] = Computer::from_be_bytes(data[index..index+16].try_into().unwrap());
-index += 16;
-}
-out
-}
-}
-#[derive(Copy, Clone)]
 pub struct Battery {
 pub charge: f32,
 }
@@ -176,19 +126,69 @@ index += 4;
 out
 }
 }
+#[derive(Copy, Clone)]
+pub struct Data {
+pub computers: [Computer; 2],
+}
+impl Data {
+pub const NAME_HASH: u32 = 1062369733;
+pub const BYTES_LENGTH: usize = 32 + 4;
+pub fn encode(&self) -> [u8; Data::BYTES_LENGTH] {
+let mut data: [u8; Data::BYTES_LENGTH] = [0; Data::BYTES_LENGTH];let mut index = 0;for x in u32::to_be_bytes(Data::NAME_HASH) {
+data[index] = x;
+index += 1;
+}
+for x in self.to_be_bytes() {
+data[index] = x;
+index += 1;
+}
+data
+}
+pub fn to_be_bytes(&self) -> [u8; Data::BYTES_LENGTH - 4] {
+let mut data: [u8; Data::BYTES_LENGTH - 4] = [0; Data::BYTES_LENGTH - 4];let mut index = 0;for i in self.computers {
+for x in i.to_be_bytes() {
+data[index] = x;
+index += 1;
+}
+}
+data
+}
+pub fn decode(data: &[u8]) -> Data {
+Data::from_be_bytes(data[4..data.len()].try_into().unwrap())
+}
+pub fn from_be_bytes(data: [u8; Data::BYTES_LENGTH - 4]) -> Data {
+let mut out = Data {
+computers: [Computer {
+batteries: [Battery {
+charge: 0.0,
+}
+; 2],
+status: Status::SLEEP,
+id: 0,
+}
+; 2],
+};
+let mut index = 0;
+for i in 0..2 {
+out.computers[i] = Computer::from_be_bytes(data[index..index+16].try_into().unwrap());
+index += 16;
+}
+out
+}
+}
 }
 }
 pub enum Structs {
-Telemetry_Data(packages::Telemetry::Data),
 Telemetry_Battery(packages::Telemetry::Battery),
 Telemetry_Computer(packages::Telemetry::Computer),
+Telemetry_Data(packages::Telemetry::Data),
 }
 pub fn decode(data: &[u8]) -> Result<Structs, &str> {
 let struct_name_hash = u32::from_be_bytes(data[0..4].try_into().unwrap());
 match struct_name_hash {
-packages::Telemetry::Data::NAME_HASH if data.len() == packages::Telemetry::Data::BYTES_LENGTH => Ok(Structs::Telemetry_Data(packages::Telemetry::Data::decode(&data))),
 packages::Telemetry::Battery::NAME_HASH if data.len() == packages::Telemetry::Battery::BYTES_LENGTH => Ok(Structs::Telemetry_Battery(packages::Telemetry::Battery::decode(&data))),
 packages::Telemetry::Computer::NAME_HASH if data.len() == packages::Telemetry::Computer::BYTES_LENGTH => Ok(Structs::Telemetry_Computer(packages::Telemetry::Computer::decode(&data))),
+packages::Telemetry::Data::NAME_HASH if data.len() == packages::Telemetry::Data::BYTES_LENGTH => Ok(Structs::Telemetry_Data(packages::Telemetry::Data::decode(&data))),
 _ => Err("Unknown data.")
 }
 }
